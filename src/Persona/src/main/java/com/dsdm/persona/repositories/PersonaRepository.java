56acb6c001b145cc7c31667132577a22b6443e6d
Sergio Villanueva <sergionicolasvillanueva@gmail.com>
2020-11-01 13:30:55 -0300

    Fix extends in generateEntityRepository.mtl

--- src/Persona/src/main/java/com/dsdm/persona/repositories/PersonaRepository.java
+++ src/Persona/src/main/java/com/dsdm/persona/repositories/PersonaRepository.java
@@ -2,10 +2,9 @@ package com.dsdm.persona.repositories;
 
 import com.dsdm.persona.entities.Persona;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PersonaRepository extends JpaRepository<Persona, Long> {
+public interface PersonaRepository extends BaseRepository<Persona, Long> {
     
 }
