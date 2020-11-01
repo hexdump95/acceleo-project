7819202e7e302a066f80b8e43cd992a93cef478d
Sergio Villanueva <65696419+hexdump95@users.noreply.github.com>
2020-11-01 13:31:55 -0300

    Fix extends in generateEntityRepository.mtl (#2)

--- src/Persona/src/main/java/com/dsdm/persona/repositories/AutorRepository.java
+++ src/Persona/src/main/java/com/dsdm/persona/repositories/AutorRepository.java
@@ -2,10 +2,9 @@ package com.dsdm.persona.repositories;
 
 import com.dsdm.persona.entities.Autor;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AutorRepository extends JpaRepository<Autor, Long> {
+public interface AutorRepository extends BaseRepository<Autor, Long> {
     
 }
--- src/Persona/src/main/java/com/dsdm/persona/repositories/LocalidadRepository.java
+++ src/Persona/src/main/java/com/dsdm/persona/repositories/LocalidadRepository.java
@@ -2,10 +2,9 @@ package com.dsdm.persona.repositories;
 
 import com.dsdm.persona.entities.Localidad;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface LocalidadRepository extends JpaRepository<Localidad, Long> {
+public interface LocalidadRepository extends BaseRepository<Localidad, Long> {
     
 }
